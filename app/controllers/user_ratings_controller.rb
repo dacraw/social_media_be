df8550ad5325075a8067e207c68feafee8933ec6
Doug <doug.a.crawford@gmail.com>
2024-09-11 01:18:47 +0200

    Write create route action for user ratings

--- app/controllers/user_ratings_controller.rb
+++ app/controllers/user_ratings_controller.rb
@@ -1,11 +1,18 @@
 class UserRatingsController < ApplicationController
     def create
+        @user_rating = UserRating.new(user_ratings_params)
+        @user_rating.rated_at = Time.now
 
+        if @user_rating.save
+            render json: @user_rating
+        else
+            render json: { error: { message: "There was an error creating the user rating." } }, status: 400
+        end
     end
 
     private
 
     def user_ratings_params
-        require(:user_rating).permit(:user_id, :rating)
+        params.require(:user_rating).permit(:user_id, :rater_id, :rating)
     end
 end
