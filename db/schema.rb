86670f7f953b37c3cd3e79968e61897726140425
Doug <doug.a.crawford@gmail.com>
2024-09-10 19:57:53 +0200

    Create Post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_10_174059) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_10_175437) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.bigint "user_id"
+    t.datetime "posted_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "user_ratings", force: :cascade do |t|
     t.bigint "user_id"
