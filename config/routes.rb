5778079e3b7bd655786b0f68a217d600ebaed203
Doug <doug.a.crawford@gmail.com>
2024-09-11 22:08:02 +0200

    Add comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :posts, only: [:create]
+  resources :comments, only: [:create]
 
   resources :user_ratings, only: [:create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
