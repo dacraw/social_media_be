a8e547526ec2d6f0b61c46497f8965b9d369174e
Doug <doug.a.crawford@gmail.com>
2024-09-11 19:58:14 +0200

    Add timeline item associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :post
+    has_many :timeline_items, as: :timelineable
 
     validates_presence_of :message, :commented_at
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
     belongs_to :user
+    has_many :timeline_items, as: :timelineable
 
     validates_presence_of :title, :body, :posted_at
 end
