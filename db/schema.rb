51370d24341fa8d4e96f84fae3acede9f5039672
Doug <doug.a.crawford@gmail.com>
2024-09-12 01:41:03 +0200

    Create GithubItem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_11_172731) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_11_232705) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_11_172731) do
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "github_events", force: :cascade do |t|
+    t.string "repo_name"
+    t.string "branch"
+    t.string "event_name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
