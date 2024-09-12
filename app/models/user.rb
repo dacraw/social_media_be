fa473b35f9f36cc30501ed247eacc35bb1b09f4f
Doug <doug.a.crawford@gmail.com>
2024-09-12 14:15:31 +0200

    Remove Devise-related logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
   validates_presence_of :email, :name, :registered_at
 
   has_many :user_ratings
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
-
   get "/users/:id/timeline", to: "users#timeline"
 
   resources :posts, only: [:create]
