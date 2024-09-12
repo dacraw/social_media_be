d40a5dc7904af688dc5a54436b32957a6c282a3c
Doug <doug.a.crawford@gmail.com>
2024-09-12 13:13:29 +0200

    Add event date to GithubEvent

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_12_110436) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_12_111236) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_12_110436) do
     t.datetime "updated_at", null: false
     t.string "github_id"
     t.bigint "user_id", null: false
+    t.datetime "date"
     t.index ["github_id"], name: "index_github_events_on_github_id"
     t.index ["user_id"], name: "index_github_events_on_user_id"
   end
