489aecfc8319554c85451c7a1950cd3bf5dfdf1c
Doug <doug.a.crawford@gmail.com>
2024-09-13 23:33:07 +0200

    Return a comment user's average rating in the json response

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,24 @@
 class CommentsController < ApplicationController
+
+    
     def create
-        @comment = Comment.new comment_params
-        @comment.commented_at = Time.now
+        post = Post.find params[:post_id]
+        
+        comment = Comment.new comment_params
+        comment.post = post
+        comment.commented_at = Time.now
 
-        if @comment.save
-            TimelineItem.create timelineable: @comment, event: TimelineItem::COMMENT_ON_POST, user: @comment.user, date: Time.now
+        if comment.save
+            TimelineItem.create timelineable: comment, event: TimelineItem::COMMENT_ON_POST, user: comment.user, date: Time.now
 
-            render json: @comment, status: 200
+            render json: comment.as_json.merge("user_average_rating" => comment.user.average_rating), status: 200
         else
-            render json: { errors: { message: @comment.errors.full_messages }}, status: 400
+            render json: { errors: { message: comment.errors.full_messages }}, status: 400
         end
     end
 
     private
     def comment_params
-        params.require(:comment).permit(:user_id, :post_id, :message)
+        params.require(:comment).permit(:user_id, :message)
     end
 end
--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -17,7 +17,12 @@ RSpec.describe "Comments", type: :request do
       expect {
         post post_comments_path(post_id: existing_post.id), params: {comment: comment_params}, headers: { authorization: "Bearer #{token}"}
 
-        expect(Comment.last.post).to eq existing_post
+        comment = Comment.last
+        
+        parsed_data = JSON.parse response.body
+        expect(parsed_data["user_id"].to_i).to eq comment.user.id
+        expect(parsed_data["post_id"].to_i).to eq comment.post.id
+        expect(parsed_data["message"]).to eq comment.message
       }.to change { Comment.count }.from(0).to(1)
     end
 
@@ -38,6 +43,26 @@ RSpec.describe "Comments", type: :request do
       expect(timeline_item.user).to eq user
       expect(timeline_item.event).to eq TimelineItem::COMMENT_ON_POST
       expect(timeline_item.timelineable).to eq Comment.last
+    end
+
+    context "when a commenter has an average rating" do
+      let(:user) { create :user, :with_ratings }
+
+      it "returns the user rating in the response" do
+        comment_params = {
+          user_id: user.id,
+          post_id: existing_post.id,
+          message: "Wow this post is so great"
+        }
+
+        token = sign_in(user)
+      
+        expect {
+          post post_comments_path(post_id: existing_post.id), params: {comment: comment_params}, headers: { authorization: "Bearer #{token}"}
+        }.to change { Comment.count }.from(0).to(1)   
+
+        expect(JSON.parse(response.body)["user_average_rating"].to_f).to eq user.average_rating
+      end
     end
   end
 end
